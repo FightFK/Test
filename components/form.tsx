c39b9921c0e7b85516e93c7675ddb5bb3d1dae87
FightFK <151952442+FightFK@users.noreply.github.com>
2025-02-27 20:35:11 +0700

    Implement Redux for form management and local storage integration

--- components/form.tsx
+++ components/form.tsx
@@ -1,40 +1,74 @@
-import React from 'react'
-import { Button, Cascader, Checkbox, ColorPicker, DatePicker, Form, Input, InputNumber, Radio, Rate, Select, Slider, Switch, TreeSelect, Upload, } from 'antd';
-const { RangePicker } = DatePicker;
-const { TextArea } = Input;
+"use client";
 
-const form = () => {
+import React from "react";
+import { Button, DatePicker, Form, Input, Radio, Select } from "antd";
+import { useDispatch } from "react-redux";
+import { addForm, resetForm } from "../src/app/redux/formSlice";
+import { nanoid } from "@reduxjs/toolkit";
+const { Option } = Select;
+
+const FormComponent = () => {
+  const dispatch = useDispatch();
+  const [form] = Form.useForm();
+
+  const handleSubmit = (values: any) => {
+    console.log(values.mobileNumber)
+    const fullMobile = `${values.mobilePrefix}${values.mobileNumber}`; // Prefix + Mobile Number
+    const newEntry = {
+      key: nanoid(),
+      name: `${values.title} ${values.firstName} ${values.lastName}`,
+      gender: values.gender,
+      mobile: fullMobile, 
+      nationality: values.nationality,
+      passport: values.passport,
+      salary: values.salary
+    };
+
+    dispatch(addForm(newEntry));
+    form.resetFields();
+  };
+  
+
+  const handleReset = () => {
+    form.resetFields();
+  };
+
   return (
-    <div style={{border:'solid',width:'1000px',height:'500px'}}>
-         <Form layout="inline">
+    <div style={{ border: "solid", width: "1000px", padding: "20px",borderRadius:'20px' }}>
+      <Form layout="inline" form={form} onFinish={handleSubmit}>
         {/* Line1 Title + Firstname + lastname*/}
-        <Form.Item label="Title">
-            <Select>
-                <Select.Option value="demo">Demo</Select.Option>
-            </Select>
+        <Form.Item label="Title" name="title" rules={[{ required: true }]}>
+          <Select style={{width:'100px'}}>
+            <Option value="Mr.">Mr.</Option>
+            <Option value="Ms.">Ms.</Option>
+          </Select>
         </Form.Item>
 
-        <Form.Item label="First Name">
-            <Input />
+        <Form.Item label="First Name" name="firstName" rules={[{ required: true }]}>
+          <Input />
         </Form.Item>
 
-        <Form.Item label="Last Name">
-            <Input />
+        <Form.Item label="Last Name" name="lastName" rules={[{ required: true }]}>
+          <Input />
         </Form.Item>
-
         {/* //Line2 Birthday + Nationality */}
         <div style={{ width: "100%" , display:'flex', marginTop:'20px'}}>
-            <Form.Item label="BirthDay">
-                <DatePicker />
+            <Form.Item label="BirthDay" name="birthday">
+            <DatePicker />
             </Form.Item>
 
-            <Form.Item label="Nationality">
-                <DatePicker />
+            <Form.Item label="Nationality" name="nationality" rules={[{ required: true }]}>
+            <Select>
+                <Option value="Thai">Thai</Option>
+                <Option value="American">American</Option>
+            </Select>
             </Form.Item>
         </div>
+
         {/* Line3 CizitenID */}
-        <div style={{ width: "100%" , display:'flex', marginTop:'20px'}} > 
-            <Form.Item label="Citizen ID:">
+
+        <div style={{ width: "100%" , display:'flex', marginTop:'20px'}}>
+            <Form.Item label="Citizen ID:" rules={[{ required: true }]}>
                 <Input.Group compact>
                     <div style={{ display: "flex", gap: "8px", alignItems: "center" }}>
                         <Input style={{ width: 60, textAlign: "center" }} maxLength={1} />
@@ -50,47 +84,62 @@ const form = () => {
                 </Input.Group>
             </Form.Item>
         </div>
-    {/* Line 4 Gender */}
-        <div style={{ width: "100%" , display:'flex', marginTop:'20px'}} >
-            <Form.Item label="Gender">
-                <Radio.Group>
-                    <Radio value="Male"> Male </Radio>
-                    <Radio value="Female"> Feamale </Radio>
-                    <Radio value="Unsex"> Unsex </Radio>
-                </Radio.Group>
-            </Form.Item>
-        </div>
 
-    {/* Line5 Mobilephone */}
+        {/* Line 4 Gender */}
+        <div style={{ width: "100%" , display:'flex', marginTop:'20px'}} >
+        <Form.Item label="Gender" name="gender" rules={[{ required: true }]}>
+          <Radio.Group>
+            <Radio value="Male"> Male </Radio>
+            <Radio value="Female"> Female </Radio>
+            <Radio value="Unsex"> Unsex </Radio>
+          </Radio.Group>
+        </Form.Item>
+
+        </div>
+     
+
+   {/* Line5 Mobilephone */}
     <div style={{ width: "100%", display: "flex", marginTop: "20px" }}>
-        <Form.Item label="Mobile Phone" style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-            <Select style={{ width: 80 }}>
-            <Select.Option value="+66">+66</Select.Option>
-            </Select>
-            <span>-</span>
-            <Input style={{ width: 150 }} maxLength={10} />
+        <Form.Item label="Mobile Phone" required>
+            <Input.Group compact>
+            <Form.Item name="mobilePrefix" noStyle>
+                <Select style={{ width: 80 }}>
+                <Select.Option value="+66">+66</Select.Option>
+                </Select>
+            </Form.Item>
+            <Form.Item name="mobileNumber" noStyle>
+              <Input style={{ width: 150 }} maxLength={9} name="mobileNumber" />
+            </Form.Item>
+           
+            </Input.Group>
         </Form.Item>
     </div>
-    {/* Line 6 Passport */}
+
+          {/* Line 6 Passport */}
     <div style={{ width: "100%", display: "flex", marginTop: "20px" }} >
-       <Form.Item label="Passport No:">
-          <Input style={{ width: 200, textAlign: "center" }} />
+       <Form.Item label="Passport No:" name="passport">
+          <Input style={{ width: '200px' }} name="passport" />
        </Form.Item>
     </div>
+
     {/* Line 7  Expected Salary + Reset Submit Buttons */}
-    <div style={{ width: "100%", display: "flex", marginTop: "20px" }} >
-       <Form.Item label="Expected Salary">
-          <Input style={{ width: 60, textAlign: "center" }} />
+    <div style={{ width: "100%", display: "flex", marginTop: "20px" , marginBottom:'20px'}} >
+       <Form.Item label="Expected Salary" name="salary">
+          <Input style={{ width: 60 }} name="salary" />
        </Form.Item>
-       <Button type="default" htmlType="reset">Reset</Button>
-        <Button type="primary" htmlType="submit">Submit</Button>
-    </div>
-    </Form>
 
     </div>
+        <Form.Item>
+          <Button type="default" onClick={handleReset}>
+            Reset
+          </Button>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
-   
-  )
-}
-
-export default form
+export default FormComponent;
--- components/table.tsx
+++ components/table.tsx
@@ -1,81 +1,76 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Table, Checkbox, Button, Space } from "antd";
 import type { ColumnsType } from "antd/es/table";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../src/app/redux/store";
+import { resetForm } from "../src/app/redux/formSlice";
 
-interface DataType {
-  key: string;
-  name: string;
-  gender: string;
-  mobile: string;
-  nationality: string;
-}
-
-const data: DataType[] = [
-  {
-    key: "1",
-    name: "test12 test12",
-    gender: "Male",
-    mobile: "+660123456789",
-    nationality: "Thai",
-  },
-  {
-    key: "2",
-    name: "test12 test12",
-    gender: "Male",
-    mobile: "+660123456789",
-    nationality: "Thai",
-  },
-];
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: <Checkbox />,
-    dataIndex: "key",
-    render: () => <Checkbox />,
-  },
-  {
-    title: "Name",
-    dataIndex: "name",
-    sorter: (a, b) => a.name.localeCompare(b.name),
-  },
-  {
-    title: "Gender",
-    dataIndex: "gender",
-  },
-  {
-    title: "Mobile Phone",
-    dataIndex: "mobile",
-  },
-  {
-    title: "Nationality",
-    dataIndex: "nationality",
-  },
-  {
-    title: "Manage",
-    dataIndex: "manage",
-    render: () => (
-      <Space>
-        <Button type="link">EDIT</Button>
-        <Button type="link" danger>
-          DELETE
-        </Button>
-      </Space>
-    ),
-  },
-];
+const LOCAL_STORAGE_KEY = "formData";
 
 const CustomTable: React.FC = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state: RootState) => state.form);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const handleClearAll = () => {
+    dispatch(resetForm());
+    if (isClient) {
+      localStorage.removeItem(LOCAL_STORAGE_KEY);
+    }
+  };
+
+  // **แสดง UI เฉพาะเมื่อเป็น client**
+  if (!isClient) return null;
+
+  const columns: ColumnsType<any> = [
+    {
+      title: <Checkbox />,
+      dataIndex: "key",
+      render: () => <Checkbox />,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+    },
+    {
+      title: "Mobile Phone",
+      dataIndex: "mobile",
+    },
+    {
+      title: "Nationality",
+      dataIndex: "nationality",
+    },
+    {
+      title: "Manage",
+      dataIndex: "manage",
+      render: () => (
+        <Space>
+          <Button type="link">EDIT</Button>
+          <Button type="link" danger>
+            DELETE
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
   return (
     <div style={{ padding: "20px" }}>
-      <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "10px" }}>
-        <Checkbox>Select All</Checkbox>
-        <Button danger>DELETE</Button>
-      </div>
-      <Table
-        columns={columns}
-        dataSource={data}
-        pagination={{ position: ["bottomRight"], pageSize: 5 }}
-      />
+      <Button onClick={handleClearAll} type="primary" danger style={{ marginBottom: "10px" }}>
+        Delete
+      </Button>
+      <Table columns={columns} dataSource={data} pagination={{ pageSize: 5 }} />
     </div>
   );
 };
